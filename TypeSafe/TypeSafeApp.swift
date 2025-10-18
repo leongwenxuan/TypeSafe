c335f9021220b515e4092ac2a4650a490f90135c
leongwenxuan <leongwenxuan0@gmail.com>
2025-10-18 09:11:17 +0800

    Initial Commit

---
+++ TypeSafe/TypeSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeSafeApp.swift
+//  TypeSafe
+//
+//  Created by Daniel on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TypeSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
